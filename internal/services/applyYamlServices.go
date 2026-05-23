9429be08d8291b1b0c39da0896d7c2907f27d78d
hakanyorulmaz <hakanyorulmaz94@gmail.com>
2026-05-23 18:10:55 +0300

    sonar fix path issue

--- internal/services/applyYamlServices.go
+++ internal/services/applyYamlServices.go
@@ -1,12 +1,17 @@
 package services_k8sclient
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 )
 
 func ApplyYaml(yamlContent string) (string, error) {
-	cmd := exec.Command("kubectl", "apply", "-f", "-")
+	kubectlPath, err := exec.LookPath("kubectl")
+	if err != nil {
+		return "", fmt.Errorf("kubectl bulunamadı: %w", err)
+	}
+	cmd := exec.Command(kubectlPath, "apply", "-f", "-")
 	cmd.Stdin = strings.NewReader(yamlContent)
 	out, err := cmd.CombinedOutput()
 	return string(out), err
